e998f353cb79bb615ab34615a46ddc447180cb61
André <andrelu0022@gmail.com>
2022-02-25 11:42:57 -0300

    Class-1

---
+++ Class-1/main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Class_1{
+    public static void main(String[] args){
+        int age1 = 21;
+        String name1 = "Andre Luiz";
+        System.out.println(name1 + " have " + age1 + " years old\n");
+
+        int age2;
+        String name2;
+        Scanner scanner = new Scanner(System.in);
+        name2 = scanner.nextLine();
+        age2 = scanner.nextInt();
+        System.out.println(name2 + " have " + age2 + " years old\n");
+        scanner.close();
+    }
+}
