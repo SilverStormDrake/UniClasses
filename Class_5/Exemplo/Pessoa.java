c43cc55f896a8dc5f8fe5a50076dd0c184476e5c
Andre Luiz Tavares da Silva - 125111366150 <125111366150@ulife.com.br>
2022-03-24 11:30:51 -0300

    Second Half for Class_5

---
+++ Class_5/Exemplo/Pessoa.java
@@ -0,0 +1,11 @@
+package Exemplo;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    double altura;
+
+    public String toString(){
+        return nome + "\n" + idade + "\n" + altura;
+    }
+}
