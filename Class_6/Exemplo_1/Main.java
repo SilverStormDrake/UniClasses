d690d86c8539d198be7e277ec15eebc46b61673a
Bruno Cesar Gennari Touzo - 125111371011 <125111371011@ulife.com.br>
2022-03-31 10:24:53 -0300

    First Half Class_6

---
+++ Class_6/Exemplo_1/Main.java
@@ -0,0 +1,25 @@
+package Class_6.Exemplo_1;
+
+public class Main {
+    public static void main(String[] args) {
+        ContaCorrente conta1 = new ContaCorrente("Ragnaldo Pereira", 77);
+        ContaCorrente conta2 = new ContaCorrente("Ximborelsson", 33);
+        
+        conta1.setSaldo(6550);
+        conta2.setSaldo(350);
+
+        conta1.sacar(400);
+        conta2.deposita(400);
+
+        conta1.sacar(7000);
+
+        conta2.deposita(6150);
+
+        System.out.println(conta2.getSaldo());
+
+        System.out.println(conta1.getSaldo());
+    }
+
+    
+    
+}
